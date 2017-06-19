0c1f963a1699bfbec0960dd62f1ccb39e2ae1519
Dante Raphael Ricalde Delgado <dante.ricalde.delgado@gmail.com>
2017-06-19 21:58:21 +0100

    paging, sorting and controlling api exposure

--- springBoot/explorecali/src/main/java/com/example/ec/repo/TourPackageRepository.java
+++ springBoot/explorecali/src/main/java/com/example/ec/repo/TourPackageRepository.java
@@ -2,6 +2,8 @@ package com.example.ec.repo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.ec.domain.TourPackage;
 
@@ -10,8 +12,34 @@ import com.example.ec.domain.TourPackage;
  * @author Dante Ricalde
  *
  */
+@RepositoryRestResource(collectionResourceRel = "packages", path="packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
 	
 	
 	TourPackage findByName(@Param(value = "name") String name);
+
+	@Override
+	@RestResource(exported=false)
+	<S extends TourPackage> S save(S entity);
+
+	@Override
+	@RestResource(exported=false)
+	<S extends TourPackage> Iterable<S> save(Iterable<S> entities);
+
+	@Override
+	@RestResource(exported=false)
+	void delete(String id);
+
+	@Override
+	@RestResource(exported=false)
+	void delete(TourPackage entity);
+
+	@Override
+	@RestResource(exported=false)
+	void delete(Iterable<? extends TourPackage> entities);
+
+	@Override
+	@RestResource(exported=false)
+	void deleteAll();
+	
 }
--- springBoot/explorecali/src/main/java/com/example/ec/repo/TourRepository.java
+++ springBoot/explorecali/src/main/java/com/example/ec/repo/TourRepository.java
@@ -2,8 +2,12 @@ package com.example.ec.repo;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.example.ec.domain.Tour;
 
@@ -12,7 +16,33 @@ import com.example.ec.domain.Tour;
  * @author Dante Ricalde
  *
  */
-public interface TourRepository extends CrudRepository<Tour, Integer> {
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
 	
-	List<Tour> findByTourPackageCode(@Param("code") String code);
+	//List<Tour> findByTourPackageCode(@Param("code") String code);
+	
+	Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
+
+	@Override
+	@RestResource(exported=false)
+	<S extends Tour> S save(S entity);
+
+	@Override
+	@RestResource(exported=false)
+	<S extends Tour> Iterable<S> save(Iterable<S> entities);
+
+	@Override
+	@RestResource(exported=false)
+	void delete(Integer id);
+
+	@Override
+	@RestResource(exported=false)
+	void delete(Tour entity);
+
+	@Override
+	@RestResource(exported=false)
+	void delete(Iterable<? extends Tour> entities);
+
+	@Override
+	@RestResource(exported=false)
+	void deleteAll();
 }
