f56f7da6218b4d5dfbcab2a66ed92e789ed7d8fc
Dante Raphael Ricalde Delgado <dante.ricalde.delgado@gmail.com>
2017-06-23 07:58:18 +0100

    finishing rest controller

--- springBoot/explorecali/src/main/java/com/example/ec/repo/TourRatingRepository.java
+++ springBoot/explorecali/src/main/java/com/example/ec/repo/TourRatingRepository.java
@@ -1,5 +1,9 @@
 package com.example.ec.repo;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -8,5 +12,22 @@ import com.example.ec.domain.TourRatingPk;
 
 @RepositoryRestResource(exported = false)
 public interface TourRatingRepository extends CrudRepository<TourRating, TourRatingPk> {
-
+	
+	/**
+	 * Lookup all the TourRatings for a tour.
+	 * 
+	 * @param tourId is the tour Identifier.
+	 * @return a List of any found TourRatings
+	 */
+	List<TourRating> findByPkTourId(Integer tourId);
+	
+	Page<TourRating> findByPkTourId(Integer tourId, Pageable pageable);
+	
+	/**
+	 * Lookup a TourRating by the TourId and Customer Id.
+	 * @param tourId
+	 * @param customerId
+	 * @return
+	 */
+	TourRating findByPkTourIdAndPkCustomerId(Integer tourId, Integer customerId);
 }
--- springBoot/explorecali/src/main/java/com/example/ec/web/TourRatingController.java
+++ springBoot/explorecali/src/main/java/com/example/ec/web/TourRatingController.java
@@ -1,8 +1,16 @@
 package com.example.ec.web;
 
+import java.util.AbstractMap;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +25,6 @@ import com.example.ec.domain.TourRating;
 import com.example.ec.domain.TourRatingPk;
 import com.example.ec.repo.TourRatingRepository;
 import com.example.ec.repo.TourRepository;
-import org.springframework.http.HttpStatus;
 /**
  * 
  * @author Dante Ricalde
@@ -46,6 +53,70 @@ public class TourRatingController {
 	public void createTourRating(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
 		Tour tour = verifyTour(tourId);
 		tourRatingRepository.save(new TourRating(new TourRatingPk(tour, ratingDto.getCustomerId()), ratingDto.getScore(), ratingDto.getComment()));
+	}
+	/*
+	@RequestMapping(method = RequestMethod.GET)
+	public List<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId) {
+		verifyTour(tourId);
+		return tourRatingRepository.findByPkTourId(tourId).stream().map(tourRating -> toDto(tourRating)).collect(Collectors.toList());
+	}*/
+	
+	// Using Pageable
+	@RequestMapping(method = RequestMethod.GET)
+	public Page<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId, Pageable pageable) {
+		verifyTour(tourId);
+		Page<TourRating> tourRatingPage = tourRatingRepository.findByPkTourId(tourId, pageable);
+		List<RatingDto>ratingDtoList = tourRatingPage.getContent().stream().map(tourRating -> toDto(tourRating)).collect(Collectors.toList());
+		return new PageImpl<RatingDto>(ratingDtoList, pageable, tourRatingPage.getTotalPages());
+	}
+	
+	@RequestMapping(path = "/average", method = RequestMethod.GET)
+	public AbstractMap.SimpleEntry<String, Double> getAverage(@PathVariable(value = "tourId") int tourId) {
+		verifyTour(tourId);
+		List<TourRating> tourRatings = tourRatingRepository.findByPkTourId(tourId);
+		OptionalDouble average = tourRatings.stream().mapToInt(TourRating::getScore).average();
+		return new AbstractMap.SimpleEntry<String, Double>("average", average.isPresent() ? average.getAsDouble() : null);
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT)
+	public RatingDto updateWithPut(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+		TourRating tourRating = verifyTourRating(tourId, ratingDto.getCustomerId());
+		tourRating.setScore(ratingDto.getScore());
+		tourRating.setComment(ratingDto.getComment());
+		return toDto(tourRatingRepository.save(tourRating));
+	}
+	
+	@RequestMapping(method = RequestMethod.PATCH)
+	public RatingDto updateWithPatch(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+		TourRating tourRating = verifyTourRating(tourId, ratingDto.getCustomerId());
+		if (ratingDto.getScore() != null) {
+			tourRating.setScore(ratingDto.getScore());			
+		}
+		if (ratingDto.getComment() != null) {
+			tourRating.setComment(ratingDto.getComment());			
+		}
+		return toDto(tourRatingRepository.save(tourRating));
+	}
+	
+	@RequestMapping(method = RequestMethod.DELETE, path = "/{customerId}")
+	public void delete(@PathVariable(value="tourId") int tourId, @PathVariable(value="customerId") int customerId) {
+		TourRating tourRating = verifyTourRating(tourId, customerId);
+		tourRatingRepository.delete(tourRating);
+	}
+	
+	/**
+	 * Verify and return the TourRating for a particular tourId and customerId
+	 * @param tourId
+	 * @param customerId
+	 * @return the found TourRating
+	 * @throws NoSuchElementException if no TourRating found
+	 */
+	private TourRating verifyTourRating(int tourId, int customerId) throws NoSuchElementException {
+		TourRating tourRating = tourRatingRepository.findByPkTourIdAndPkCustomerId(tourId, customerId);
+		if (tourRating == null) {
+			throw new NoSuchElementException("Tour-Rating pair for request(" + tourId + " for customer " + customerId);
+		}
+		return tourRating;
 	}
 	
 	/**
