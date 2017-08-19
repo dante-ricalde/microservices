ae2fff66eff9d480f62a5acd7d756d937236893a
Dante Ricalde <dante.ricalde.delgado@gmail.com>
2017-08-19 08:54:18 -0500

    configuring kubernetes

---
+++ springBoot/redhat-microservices-by-example/hola-springboot/src/main/java/com/redhat/examples/web/HolaRestController.java
@@ -0,0 +1,42 @@
+package com.redhat.examples.web;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @author dante
+ *
+ */
+@RestController
+@RequestMapping("/api")
+@ConfigurationProperties(prefix = "helloapp")
+public class HolaRestController {
+	
+	private String saying;
+
+	@RequestMapping(method = RequestMethod.GET, value = "/hola", produces = "text/plain")
+	public String hola() throws UnknownHostException {
+		String hostname = null;
+		try {
+			hostname = InetAddress.getLocalHost().getHostAddress();
+		} catch (Exception e) {
+			hostname = "Unknown";
+		}
+		return saying + " " + hostname; 
+	}
+
+	public String getSaying() {
+		return saying;
+	}
+
+	public void setSaying(String saying) {
+		this.saying = saying;
+	}
+
+}
