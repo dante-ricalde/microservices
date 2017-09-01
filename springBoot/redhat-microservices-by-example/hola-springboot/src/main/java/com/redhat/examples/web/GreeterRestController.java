b421477651791943f581436de55286a0f63ec1ec
Dante Ricalde <dante.ricalde.delgado@gmail.com>
2017-09-01 07:49:22 -0500

    configuring load balancing with Ribbon using kubernetes discovery

--- springBoot/redhat-microservices-by-example/hola-springboot/src/main/java/com/redhat/examples/web/GreeterRestController.java
+++ springBoot/redhat-microservices-by-example/hola-springboot/src/main/java/com/redhat/examples/web/GreeterRestController.java
@@ -5,16 +5,29 @@ import java.net.UnknownHostException;
 
 import javax.annotation.PostConstruct;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.client.config.DefaultClientConfigImpl;
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.ILoadBalancer;
+import com.netflix.loadbalancer.LoadBalancerBuilder;
+
+import io.fabric8.kubeflix.ribbon.KubernetesServerList;
+
 @RestController
 @RequestMapping("/api")
 @ConfigurationProperties(prefix = "greeting")
 public class GreeterRestController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(GreeterRestController.class);
 
 	private RestTemplate template = new RestTemplate();
 
@@ -23,10 +36,26 @@ public class GreeterRestController {
 	private String backendServiceHost;
 
 	private int backendServicePort;
+	
+	@Value(value = "${USE_KUBERNETES_DISCOVERY:'false'}")
+	private String useKubernetesDiscovery;
+	
+	private ILoadBalancer loadBalancer;
+	private IClientConfig config;
+	
+//	@Value("${server.port}")
+//    private String serverPort;
 
 	@PostConstruct
 	public void init() {
 		saying += " from cluster hola-springboot at host: " + getIp();
+		System.out.println("Value of USE_KUBERNETES_DISCOVERY: " + useKubernetesDiscovery);
+		this.config = new DefaultClientConfigImpl();
+		this.config.loadProperties("backend");
+		if (Boolean.valueOf(useKubernetesDiscovery)) {
+			logger.debug("Using Kubernetes discovery for ribbon ...");
+			loadBalancer = LoadBalancerBuilder.newBuilder().withDynamicServerList(new KubernetesServerList(config)).buildDynamicServerListLoadBalancer();
+		}
 	}
 
 	@RequestMapping(value = "/greeting", method = RequestMethod.GET, produces = "text/plain")
@@ -44,6 +73,11 @@ public class GreeterRestController {
 		BackendCommand backendCommand = new BackendCommand(backendServiceHost, backendServicePort).withSaying(saying).withTemplate(template);
 		BackendDTO response = backendCommand.execute();
 		return response.getGreeting() + " at host: " + response.getIp();
+	}
+	
+	@GetMapping(value = "/greeting-ribbon", produces = "text/plain")
+	public String greetingRibbon() {
+		return null;
 	}
 
 	public String getSaying() {
@@ -78,6 +112,14 @@ public class GreeterRestController {
 			hostname = "unknown";
 		}
 		return hostname;
+	}
+
+	public String getUseKubernetesDiscovery() {
+		return useKubernetesDiscovery;
+	}
+
+	public void setUseKubernetesDiscovery(String useKubernetesDiscovery) {
+		this.useKubernetesDiscovery = useKubernetesDiscovery;
 	}
 
 }
--- springBoot/redhat-microservices-by-example/hola-springboot/src/test/java/com/redhat/examples/HolaSpringbootApplicationTests.java
+++ springBoot/redhat-microservices-by-example/hola-springboot/src/test/java/com/redhat/examples/HolaSpringbootApplicationTests.java
@@ -2,15 +2,24 @@ package com.redhat.examples;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HolaSpringbootApplicationTests {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HolaSpringbootApplicationTests.class);
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testBoolean() {
+		Boolean value = Boolean.valueOf("kk");
+		logger.debug("Boolean value: " + value);
+	}
 }
