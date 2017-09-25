b57d69d6d2f409b4ff8bb0a1397706299dc6d27a
Dante Ricalde <dante.ricalde.delgado@gmail.com>
2017-09-25 07:57:46 -0500

    full config server

---
+++ configserver/src/main/java/com/spring/cloud/config/server/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud.config.server;
+
+/**
+ * 
+ * @author dante
+ *
+ */
+public class ConfigServerApplication {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
