6df3dead4cfacb66bbf20eff3499698a57719afe
Dante Ricalde <dante.ricalde.delgado@gmail.com>
2017-10-16 08:12:12 -0500

    microservices stores. Reading csv

---
+++ customers-stores/rest-microservices-store/src/main/java/com/spring/cloud/samples/rest/rest_microservices_store/StoreInitializer.java
@@ -0,0 +1,82 @@
+package com.spring.cloud.samples.rest.rest_microservices_store;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.geo.Point;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BindException;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Component initializing a hand full of Starbucks stores and persisting them through a {@link StoreRepository}.
+ * 
+ *
+ */
+@Slf4j
+@Component
+public class StoreInitializer {
+
+	@Autowired
+	public StoreInitializer(StoreRepository repository, MongoOperations operations) throws Exception {
+		if (repository.count() != 0) {
+			return;
+		}
+		List<Store> stores = readStores();
+	}
+	
+	/**
+	 * Reads a file {@code starbucks.csv} from the class path and parses it into {@link Store} instances
+	 * about to persisted.
+	 *  
+	 * @return
+	 * @throws Exception
+	 */
+	public static List<Store> readStores() throws Exception {
+		ClassPathResource resource = new ClassPathResource("starbucks.csv");
+		Scanner scanner = new Scanner(resource.getInputStream());
+		String line = scanner.nextLine();
+		scanner.close();
+		
+		FlatFileItemReader<Store> itemReader = new FlatFileItemReader<>();
+		itemReader.setResource(resource);
+		
+		// DelimitedLineTokenizer defaults to comma as its delimiter
+		DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+		tokenizer.setNames(line.split(","));
+		tokenizer.setStrict(false);
+		
+		DefaultLineMapper<Store> lineMapper = new DefaultLineMapper<>();
+		lineMapper.setLineTokenizer(tokenizer);
+		lineMapper.setFieldSetMapper(StoreFieldSetMapper.INSTANCE);
+		
+		
+		
+		List<Store> stores = new ArrayList<>();
+		
+		return stores;
+	}
+	
+	private static enum StoreFieldSetMapper implements FieldSetMapper<Store> {
+		
+		INSTANCE;
+		
+		@Override
+		public Store mapFieldSet(FieldSet fields) throws BindException {
+			Point location = new Point(fields.readDouble("Longitude"), fields.readDouble("Latitude"));
+			Address address = new Address(fields.readString("Street Address"), fields.readString("City"), fields.readString("Zip"), location);
+			return new Store(fields.readString("Name"), address);
+		}
+	}
+}
